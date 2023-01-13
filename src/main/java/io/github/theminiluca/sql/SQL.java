35720f0920572e536a00d4cb55f5352c09e26c42
TheMiniLuca <TheMiniLuca@gmail.com>
2023-01-13 22:05:36 +0900

    fixed untracked files

---
+++ src/main/java/io/github/theminiluca/sql/SQL.java
@@ -0,0 +1,16 @@
+package io.github.theminiluca.sql;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+public @interface SQL {
+
+    boolean enable() default true;
+
+    boolean primary() default false;
+
+}
---
+++ src/main/java/io/github/theminiluca/sql/SQLObject.java
@@ -0,0 +1,8 @@
+package io.github.theminiluca.sql;
+
+import java.util.HashMap;
+
+public interface SQLObject {
+
+    public void saveSQL();
+}
